a1d0b9c08b8d50d83e29d8f063db2b323779ee49
Ansible <ansible@ansible.hearn.local>
2017-12-21 11:19:30 -0500

    made it actually look for --host/--list

--- spacewalk_inventory_json.py
+++ spacewalk_inventory_json.py
@@ -1,53 +1,51 @@
 #!/usr/bin/python
 import xmlrpclib
 import json
+import sys
 
-SATELLITE_URL = "http://spacewalk.hearn.local/rpc/api"
-SATELLITE_LOGIN = "admin"
-SATELLITE_PASSWORD = "shitcockhorsebacon"
+if len(sys.argv) != 2:
+  print "Usage: " + sys.argv[0] + "[--host | --list]"
+  exit(1)
 
-client = xmlrpclib.Server(SATELLITE_URL, verbose=0)
+if sys.argv[1] == "--host":
+  print "{}"
 
-key = client.auth.login(SATELLITE_LOGIN, SATELLITE_PASSWORD)
-grouplist = client.systemgroup.listAllGroups(key)
+if sys.argv[1] == "--list":
+  SATELLITE_URL = "http://spacewalk.hearn.local/rpc/api"
+  SATELLITE_LOGIN = "admin"
+  SATELLITE_PASSWORD = "shitcockhorsebacon"
 
-print "{"
-print "	\"_meta\": {"
-print "		\"hostvars\": {}"
-print "	}"
+  client = xmlrpclib.Server(SATELLITE_URL, verbose=0)
+  
+  key = client.auth.login(SATELLITE_LOGIN, SATELLITE_PASSWORD)
+  grouplist = client.systemgroup.listAllGroups(key)
 
-for group in grouplist:  #[:-1]:
-  groupname=group.get('name')
-  print "	\"" + groupname + "\": {"
-  #print "[" + groupname.lower() + "]"
-  systemlist = client.systemgroup.listSystems(key, groupname)
-  print "		\"hosts\":"
+  print "{"
+  print " \"_meta\": {"
+  print "         \"hostvars\": {}"
+  print " }"
+
+  for group in grouplist:
+    groupname=group.get('name')
+    print "       \"" + groupname + "\": {"
+    #print "[" + groupname.lower() + "]"
+    systemlist = client.systemgroup.listSystems(key, groupname)
+    print "               \"hosts\":"
+    systemnames=[]
+    for system in systemlist:
+      systemnames.append(system.get('profile_name'))
+      #print system.get('profile_name')
+    print json.dumps(systemnames)
+    print "       },"
+
+  print " \"Ungrouped\": {"
+  systemlist = client.system.listUngroupedSystems(key)
   systemnames=[]
   for system in systemlist:
-    systemnames.append(system.get('profile_name'))
-    #print system.get('profile_name')
+    systemnames.append(system.get('name'))
+
   print json.dumps(systemnames)
-  print "	},"
-
-#group=grouplist[-1]
-#groupname=group.get('name')
-#print "	\"" + groupname + "\": {"
-##print "[" + groupname.lower() + "]"
-#print "		\"hosts\":"
-#systemlist = client.systemgroup.listSystems(key, groupname)
-#systemnames=[]
-#for system in systemlist:
-  #systemnames.append(system.get('profile_name'))
-  #print system.get('profile_name')
-
-print "	\"Ungrouped\": {"
-systemlist = client.system.listUngroupedSystems(key)
-systemnames=[]
-for system in systemlist:
-  systemnames.append(system.get('name'))
-
-print json.dumps(systemnames)
-print "	}"
-
-print "}"
-client.auth.logout(key)
+  print " }"
+  
+  print "}"
+  client.auth.logout(key)
